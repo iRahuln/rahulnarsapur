78105f3f6f72eadc9a565875b18332891b4e23db
iRahuln <oyyweutyet@gmail.com>
2025-10-10 01:52:15 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import path from "path";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   base: '/rahulnarsapur/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     outDir: 'dist'
   }
